3aabcf94fba0c7678ccebc5bff4359a5a0f92380
Krista Loven <krista.loven.87@gmail.com>
2019-10-14 12:56:45 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const win = array.find(superbowl => superbowl.result === "W")
+  if (win) {
+    return win.year
+  }
+}
